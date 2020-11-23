9bf71d7e3cefaa737e4fbfbdc8e662df1c4cf63d
Kim-Ju-Yeop <kjy031104@gmail.com>
2020-11-24 01:36:31 +0900

    [ADD] Bottom Navigation
    MainActivity 내의 하단바를 구성했습니다.

--- presentation/src/main/java/kr/hs/dgsw/juyeop/mymap/view/activity/MainActivity.kt
+++ presentation/src/main/java/kr/hs/dgsw/juyeop/mymap/view/activity/MainActivity.kt
@@ -1,5 +1,10 @@
 package kr.hs.dgsw.juyeop.mymap.view.activity
 
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
+import kr.hs.dgsw.juyeop.mymap.R
 import kr.hs.dgsw.juyeop.mymap.base.view.BaseActivity
 import kr.hs.dgsw.juyeop.mymap.databinding.ActivityMainBinding
 import kr.hs.dgsw.juyeop.mymap.viewmodel.activity.MainViewModel
@@ -14,6 +19,13 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
 
     override val viewModel: MainViewModel
         get() = getViewModel(viewModelFactory)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val navController = findNavController(R.id.page_fragment)
+        bottom_navigation_view.setupWithNavController(navController)
+    }
 
     override fun observerViewModel() {}
 }
