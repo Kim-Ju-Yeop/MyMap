f2735f2e8cd5c90451a43536690c35a8d2fba677
Kim-Ju-Yeop <kjy031104@gmail.com>
2020-11-24 16:19:56 +0900

    [ADD] SignInActivity
    로그인 화면을 구성했습니다.

--- presentation/src/main/java/kr/hs/dgsw/juyeop/mymap/view/activity/SignInActivity.kt
+++ presentation/src/main/java/kr/hs/dgsw/juyeop/mymap/view/activity/SignInActivity.kt
@@ -1,10 +1,14 @@
 package kr.hs.dgsw.juyeop.mymap.view.activity
 
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.activity_sign_in.*
 import kr.hs.dgsw.juyeop.mymap.base.view.BaseActivity
 import kr.hs.dgsw.juyeop.mymap.databinding.ActivitySignInBinding
 import kr.hs.dgsw.juyeop.mymap.viewmodel.activity.SignInViewModel
 import kr.hs.dgsw.juyeop.mymap.viewmodelfactory.activity.SignInViewModelFactory
 import kr.hs.dgsw.juyeop.mymap.widget.extension.getViewModel
+import kr.hs.dgsw.juyeop.mymap.widget.extension.shortSnackbar
+import kr.hs.dgsw.juyeop.mymap.widget.extension.startActivityWithFinish
 import javax.inject.Inject
 
 class SignInActivity : BaseActivity<ActivitySignInBinding, SignInViewModel>() {
@@ -15,5 +19,20 @@ class SignInActivity : BaseActivity<ActivitySignInBinding, SignInViewModel>() {
     override val viewModel: SignInViewModel
         get() = getViewModel(viewModelFactory)
 
-    override fun observerViewModel() {}
+    override fun observerViewModel() {
+        with(viewModel) {
+            onSignInEvent.observe(this@SignInActivity, Observer {
+                postLogin()
+            })
+            onSignUpEvent.observe(this@SignInActivity, Observer {
+                shortSnackbar(layout, "준비중인 기능입니다.")
+            })
+            onSuccessEvent.observe(this@SignInActivity, Observer {
+                startActivityWithFinish(applicationContext, MainActivity::class.java)
+            })
+            onFailureEvent.observe(this@SignInActivity, Observer {
+                shortSnackbar(layout, "아이디 또는 비밀번호가 틀렸습니다.")
+            })
+        }
+    }
 }
--- presentation/src/main/java/kr/hs/dgsw/juyeop/mymap/viewmodel/activity/SignInViewModel.kt
+++ presentation/src/main/java/kr/hs/dgsw/juyeop/mymap/viewmodel/activity/SignInViewModel.kt
@@ -1,32 +1,50 @@
 package kr.hs.dgsw.juyeop.mymap.viewmodel.activity
 
 import android.content.Context
+import androidx.lifecycle.MutableLiveData
 import io.reactivex.observers.DisposableSingleObserver
 import kr.hs.dgsw.juyeop.data.util.SharedPreferencesManager
 import kr.hs.dgsw.juyeop.domain.entity.Auth
 import kr.hs.dgsw.juyeop.domain.request.LoginRequest
 import kr.hs.dgsw.juyeop.domain.usecase.PostLoginUseCase
 import kr.hs.dgsw.juyeop.mymap.base.viewmodel.BaseViewModel
+import kr.hs.dgsw.juyeop.mymap.widget.SingleLiveEvent
 
 class SignInViewModel(
     private val context: Context,
     private val postLoginUseCase: PostLoginUseCase
 ) : BaseViewModel() {
 
-    init {
-        postLogin()
-    }
+    val email = MutableLiveData<String>()
+    val password = MutableLiveData<String>()
 
-    private fun postLogin() {
-        val loginRequest = LoginRequest("asdf", "asdf")
+    val onSignInEvent = SingleLiveEvent<Unit>()
+    val onSignUpEvent = SingleLiveEvent<Unit>()
 
+    val onSuccessEvent = SingleLiveEvent<Unit>()
+    val onFailureEvent = SingleLiveEvent<Unit>()
+
+    fun postLogin() {
+        isLoading.value = true
+        val loginRequest = LoginRequest(email.value.toString(), password.value.toString())
         addDisposable(postLoginUseCase.buildUseCaseObservable(PostLoginUseCase.Params(loginRequest)), object : DisposableSingleObserver<Auth>() {
             override fun onSuccess(auth: Auth) {
+                isLoading.value = false
                 SharedPreferencesManager.setToken(context, auth.access_token)
+                onSuccessEvent.call()
             }
             override fun onError(e: Throwable) {
+                isLoading.value = false
+                onFailureEvent.call()
                 e.printStackTrace()
             }
         })
+    }
+
+    fun signInEvent() {
+        onSignInEvent.call()
+    }
+    fun signUpEvent() {
+        onSignUpEvent.call()
     }
 }
