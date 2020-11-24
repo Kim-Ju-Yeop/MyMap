8a9a3f2d308ddcc836eb77a328da4eee44a2653d
Kim-Ju-Yeop <kjy031104@gmail.com>
2020-11-24 10:44:50 +0900

    [ADD] HomeFragment UI
    HomeFragment TimeLine UI를 구성했습니다.

---
+++ presentation/src/main/java/kr/hs/dgsw/juyeop/mymap/view/adapter/SpotItemAdapter.kt
@@ -0,0 +1,17 @@
+package kr.hs.dgsw.juyeop.mymap.view.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import kr.hs.dgsw.juyeop.mymap.databinding.ItemSpotBinding
+import kr.hs.dgsw.juyeop.mymap.viewmodel.adapter.SpotItemViewModel
+
+class SpotItemAdapter : RecyclerView.Adapter<>() {
+
+
+    class ViewHolder(val binding: ItemSpotBinding): RecyclerView.ViewHolder(binding.root) {
+        val viewModel = SpotItemViewModel()
+
+        fun bind() {
+
+        }
+    }
+}
---
+++ presentation/src/main/java/kr/hs/dgsw/juyeop/mymap/viewmodel/adapter/SpotItemViewModel.kt
@@ -0,0 +1,5 @@
+package kr.hs.dgsw.juyeop.mymap.viewmodel.adapter
+
+import kr.hs.dgsw.juyeop.mymap.base.viewmodel.BaseViewModel
+
+class SpotItemViewModel : BaseViewModel()
